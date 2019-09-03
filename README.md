c428cc2bbf8f27804c9943253439b99c516f38ab
eDriiz <sirdris@gmail.com>
2019-09-03 16:34:07 +0100

    My Host Resume

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sado Idris Erick
+
+This is a link to my lucid blog stating what I have learnt so far from the hng internship
+https://lucid.blog/sirdris/post/so-far-so-good-cb2
+
+This is a link to the tweet I made
+https://twitter.com/eDriiz/status/1164354200748396546
+
+This is the link to my lucid blog in which I talked on how CSS has made HTML better
+https://lucid.blog/sirdris/post/css-the-html-beauty-79b
---
+++ main.js
@@ -0,0 +1,79 @@
+console.log('it works')
+
+$(document).ready(function () {
+    $('.submit').click(function (event){
+      
+console.log('Clicked button')
+     
+
+        var name = $('.name').val()
+            var email = $('.email').val()
+            var title = $('.title').val()
+            var message = $('.message').val()
+            var statusElm = $('.status')
+      
+            statusElm.empty()
+
+            
+
+       
+    
+        
+        if (name.length <= 0) {
+            alert('Name field cannot be empty')
+           
+        }
+        else {
+            event.preventDefault()
+            statusElm.append('Name is valid')}
+
+        if (email.length > 2 && email.includes('@') && email.includes('.')) {
+            statusElm.append('Email is valid')
+        }
+        else {
+            event.preventDefault()
+            alert('Email is not valid')
+
+        }
+
+
+
+        if (title.length >= 1) {
+            statusElm.append ('title is valid')
+        }
+        else {
+            event.preventDefault()
+            alert('Title field cannot not empty')
+        }
+      
+    
+      
+   if (message.length >= 20) {
+            statusElm.append('<div>Your message is valid</div>')
+        } else {
+            event.preventDefault()
+            alert('Please message needs to be above 20 words')
+        }
+    
+
+
+        
+    
+
+     
+   
+           
+          
+          
+
+        
+       
+          
+     
+
+        
+      
+         
+
+    })
+})
