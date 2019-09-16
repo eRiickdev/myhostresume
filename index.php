d39ecca7055161ee88b79abacf800d93ae6cfe8f
sirdris <54279625+sirdris@users.noreply.github.com>
2019-09-16 17:10:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,593 @@
+
+    
+    <html>
+  
+    
+    <head>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Primary Meta Tags -->
+    <title>Sado Idris Erick |  Web Backend Developer | Computer Engr.</title>
+    <meta name="title" content=" Sado Idris Erick |  Web Backend Developer | Computer Engr.">
+    <meta name="description" content="A Tech solution driven,Computer Maintenance and Repairs, Web Backend Developer.
+    ">
+    <meta name="keywords"
+        content="Sado, Sado Erick, Sado Idris Erick,
+         backend developer, backend developer in Lagos, best backend developer in Lagos, backend developers in Lagos, poet, poetry, poets in lagos, poets in Nigeria">
+
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="7 days">
+    <meta name="author" content="Sado Idris Erick">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:title" content="Sado Idris Erick | Web Backend Developer | Computer Engr.">
+    <meta property="og:description" content="A Tech solution driven,Computer Maintenance and Repairs,  Web Backend Development, Poet, Photographer.
+    ">
+    <meta property="og:image"
+      
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="">
+    <meta property="twitter:title" content="Sado Idris Erick |  Web Backend Developer | Computer Engr.">
+    <meta property="twitter:description" content="A Tech solution driven,Computer Maintenance and Repairs, Web Backend Development, Poet, Photographer           .
+    ">
+    <meta property="twitter:image"
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="contact.css">
+
+
+        <title>My Resume</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+     
+ 
+    
+  
+    
+    <style>
+      
+            html {
+                box-sizing: border-box;
+            }
+    
+    
+            body {
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                line-height: 1.5;
+                background: #f2f2f2;
+                color: #323232;
+            }
+    
+            img {
+                max-width: 100%;
+            }
+    
+            .container {
+                max-width: 960px;
+                margin: 40px auto;
+                padding: 32px;
+                background: white;
+                box-shadow: 0 2px 4px rgba(red, green, blue, alpha)
+            }
+    
+            section {
+                display: grid;
+                grid-template-columns: 1fr 4fr;
+                grid-gap: 20px;
+                padding: 24px 0;
+                border-bottom: 1px solid lightgrey;
+            }
+    
+            section :last-child {
+                border-bottom: none;
+            }
+    
+            .section-title {
+                font-weight: bold;
+                font-size: 18px;
+    
+            }
+    
+            img.image {
+                width: 130px;
+            }
+    
+            .myname {
+                font-size: 48px;
+                line-height: 1;
+            }
+    
+            .detail {
+                font-size: 24px;
+                font-weight: 300;
+                color: blue;
+            }
+    
+            .mail {
+                font-size: 18px;
+                font-weight: 300;
+                color: blueviolet;
+                font-style: italic;
+    
+    
+            }
+    
+            .sub {
+                font-size: 25px;
+                line-height: 1;
+                text-decoration: underline;
+                margin-top: 10px;
+            }
+    
+    
+            .job {
+                padding-bottom: 24px;
+                margin-bottom: 24px;
+                border-bottom: 1px solid lightgrey;
+            }
+    
+            job:last-child {
+                border-bottom: none;
+            }
+    
+            .job-title-container {
+                display: flex;
+                justify-content: space-between;
+                margin-bottom: 40px;
+                font-size: 18px;
+            }
+    
+            .job-company {
+                font-weight: bold;
+            }
+    
+            .job-company {
+                font-weight: bold;
+                font-size: 15px;
+    
+    
+            }
+    
+            .contact {
+                color: red;
+                font-style: italic;
+    
+            }
+
+            .page {
+	 width: 400px;
+    background: rgb(162, 31, 31);
+    margin: 200px left;
+    padding: 40px;
+    border-radius: 30px;
+    height: 400px;
+    margin-left:400px;
+    margin-top: 1rem;
+    margin-right: 5rem;
+}
+button{
+    background: #E1BEE7;
+    text-align: center;
+    padding: 20px;
+    margin-left: 30px;
+    margin-bottom: 100px;
+    border-radius: 100px;
+  
+}
+.body{
+
+    margin-bottom: 300px;
+    padding-bottom: 300px;
+}
+
+
+
+        </style>
+  
+ <script>
+     function validateform() {
+            var name = document.myform.name.value;
+          
+             var x = document.myform.email.value;
+
+    
+         var atposition = x.indexOf("@");
+         var dotposition = x.lastIndexOf(".");  
+
+         var Title = document.myform.Title.value;
+
+          var Message= document.myform.Message.value;
+
+         
+          
+        
+            if (name == null || name == "")
+             {
+                alert("Name can't be blank");
+                return false;
+            }
+             else if (name.length < 4)
+              {
+                alert("Name must be at least 4 characters long.");
+                return false;
+            }
+            if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+             alert("Please enter a valid e-mail address");
+             return false;
+         }
+         
+          if (Title == null || Title == "") {
+             alert("Title can't be blank");
+             return false;
+         }
+            if (Message == null || Message == "") {
+             alert("Message can't be blank");
+             return false;
+         }
+         else if (Message.length <= 20) {
+             alert("Name must be at least 20 characters long.");
+             return false;}
+
+  
+        }  
+    
+          
+        
+  
+
+    </script>
+
+</head>
+
+<body>
+  
+    
+    
+    
+    </head>
+    
+    
+    <body>
+    
+        <div class="container">
+    
+            <section>
+    
+                <div><img
+                        src="https://res.cloudinary.com/eriick/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1566684387/20190824_225953_1_vflsjw.jpg"
+                        class="image"></div>
+    
+                <header align="center">
+                    <div class="myname">SADO IDRIS ERICK</div>
+    
+                    <div class="detail">No. 8 Bakare Street Langbasa,Owode,</div>
+                    <div class="detail">Ajah,Lagos.</div>
+                    <div class="detail">07036270893</div>
+                    <div class="mail">sirdris@gmail.com</div>
+    
+    
+    
+    
+                    <div class="sub">
+    
+                        PERSONAL PROFILE
+    
+                    </div>
+                    I am a hardworking, enthusiastic person who has over the years developed a mature and responsible
+                    approach to any task I
+                    handle.
+                    Having had years of experience in management, I am excellent in working with others to achieve a
+                    certain objectiveon
+                    time and with excellence
+    
+                </header>
+    
+            </section>
+    
+        </div>
+    
+        <section>
+    
+            <div class="section-title">WORK EXPERIENCE</div>
+    
+            <div>
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">Luisant Blu Limited</div>
+    
+                            <div class="job-title">ADMIN PERSONNEL</div>
+    
+                        </div>
+    
+                        <div>Dec 2017 till date</div>
+    
+    
+    
+                    </div>
+                    <ul>
+    
+    
+                        <li>Maintenance and update of company's database</li>
+                        <li>Booking of manager's meetings as required</li>
+                        <li>Organize and file confidential company documents</li>
+                        <li>Schedule in-house and external meetings</li>
+                        <li>Tracking meetings and reminders for management staff</li>
+    
+                    </ul>
+                    <!--job container-->
+                </div>
+                <!--end job-->
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">Mcoy Green Energy Limited</div>
+    
+                            <div class="job-title">FRONT DESK PERSONNEL</div>
+    
+                        </div>
+    
+                        <div>May 2015 - August 2017</div>
+    
+    
+    
+    
+    
+                    </div>
+                    <!--job container-->
+                    <ul>
+    
+    
+                        <li>Ensure warm reception and comfort of every guest</li>
+                        <li>Operate switch boards and take all phone calls</li>
+                        <li>Answer inquiries about the company</li>
+                        <li>Ensure reception area is tidy at all times</li>
+                        <li>Receive and distribute mails</li>
+    
+    
+                    </ul>
+                </div>
+                <!--end job-->
+    
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">El-Mark Construction Company</div>
+    
+                            <div class="job-title">PERSONAL ASSISTANCE TO DIRECTOR</div>
+    
+                        </div>
+    
+                        <div>January 2013 - July 2015</div>
+    
+    
+    
+                    </div>
+                    <!--job container-->
+                    <ul>
+    
+    
+                        <li>Book and arrange travels and accomodation</li>
+                        <li>Carry out miscellaneous tasks as may be required by the director</li>
+                        <li> reminders for meetings and appointments</li>
+                        <li>Handle emails and correspondents</li>
+    
+                    </ul>
+                </div>
+                <!--end job-->
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">Dream World Africana Leisure Park</div>
+    
+                            <div class="job-title">VISITOR CARE OFFICER</div>
+    
+                        </div>
+    
+                        <div>November 2009 - November 2012</div>
+    
+    
+                    </div>
+                    <!--job container-->
+                    <ul>
+    
+    
+                        <li>Ensure visitors apply all safety rules and measures</li>
+                        <li>Keep an eye on under age children</li>
+                        <li>Intimate guests on with the rules of the rides available in the amusement area</li>
+                        <li>Report and incidents and complaints to the management immediately</li>
+    
+                    </ul>
+                </div>
+                <!--end job-->
+    
+            </div>
+        </section>
+    
+        <section>
+    
+            <div class="section-title">ACADEMIC QUALIFICATIONS</div>
+            <div>
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">APTECH COMPUTER EDUCATION</div>
+    
+                            <div class="job-title">Credit</div>
+    
+                        </div>
+    
+                        <div>2015-2017</div>
+    
+                    </div>
+    
+                </div>
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">MCL</div>
+    
+                            <div class="job-title">Customer Service Excellence</div>
+    
+                        </div>
+    
+                        <div>2011</div>
+    
+                    </div>
+    
+                </div>
+    
+    
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">LEXY COMPUTERS</div>
+    
+                            <div class="job-title">Computer Engineering</div>
+    
+                        </div>
+    
+                        <div>2002 - 2004</div>
+    
+                    </div>
+    
+                </div>
+                <div class="job">
+    
+                    <div class="job-title-container">
+    
+                        <div>
+    
+                            <div class="job-company">OYEMEKUN GRAMMAR SCHOOL</div>
+    
+                            <div class="job-title">SSCE</div>
+    
+                        </div>
+    
+                        <div>1995 - 2001</div>
+    
+                    </div>
+    
+                </div>
+    
+            </div>
+    
+            </div>
+    
+    
+        </section>
+    
+    
+        <section>
+    
+            <div class="section-title">SKILLS</div>
+    
+            <div>
+    
+                <div class="skills-container">
+    
+                    <ul>
+    
+                        <li>Proficiency in HTML and CSS</li>
+                        <li>Troubleshooting and fixing major and minor computer hardware and software issues</li>
+                        <li>Excellent customer relations</li>
+                        <li>Good team player</li>
+    
+                    </ul>
+    
+                </div>
+    
+            </div>
+    
+        </section>
+    
+        <section>
+    
+            <div class="section-title">REFREES</div>
+    
+            Available upon request
+    
+    
+    
+        </section>
+        <div class = "page">
+
+
+<form name="myform" method="post" action="" onsubmit="return validateform()">
+ <div class="contact-us"> <center> <p> <h3>Contact Me</h3></p></center>
+   
+
+<center><br><input type="text"    name="name" style="width:400px" ; height="500px" placeholder="Name"><br></center>
+   
+<center><br><input type="text" name="email" style="width:400px" ; height="150px" placeholder="Email"><br></center>
+
+<center><br><input type="text" name="Title" style="width:400px"; height="150px" placeholder="Title"></center><br>
+
+
+<center><br><textarea input type="text" cols="30" rows="10" name="Message" style="width:400px" ; height="150px" placeholder="Message"></textarea> </center> <br>
+   
+
+   
+
+    
+   
+
+
+<center><button input type="submit" value="Submit"><b>submit</b></button></center>
+</form>
+
+</div>
+
+
+
+</body>
+
+</html>
+
+  <?php
+      
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['Title']) && isset($_POST['Message'])) {
+   
+$fp = fopen("data.txt", "a");
+$savestring = $_POST['name'] . ' ' . $_POST['email'] . "\n" .$_POST['Title'] . "\n" . $_POST['Message'];
+fwrite($fp, $savestring);
+fclose($fp);
+}
+     
+      ?>
